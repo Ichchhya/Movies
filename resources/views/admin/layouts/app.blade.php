992ac3be55ab0655bcfd068a791aca226fb8e7c8
Ichchhya <samikshyadhakal21@gmail.com>
2025-03-16 10:00:54 +0545

    update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>Movies</title>
+  <title>Movie List</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
